aaa898658bd706a07e41252e24ad536bbeba2c82
piovarcisimon <piovarcisimon@HPGYRI-028>
2026-03-12 13:12:00 +0100

    Přidejte soubory projektu.

---
+++ Kalkulacka/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Kalkulacka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(273, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtDisplay.Location = new System.Drawing.Point(273, 101);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(200, 26);
+            this.txtDisplay.TabIndex = 1;
+            this.txtDisplay.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtDisplay.MouseMove += new System.Windows.Forms.MouseEventHandler(this.txtDisplay_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 454);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Moje Kalkulačka";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtDisplay;
+    }
+}
+
---
+++ Kalkulacka/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulacka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            txtDisplay.Text = "Ahoj světe";
+            ///do txtdisplay s vlastnosti text mi to napise ahoj svete
+         
+       
+            ///mam to naprogramovat tak, aby policko pak bylo zelene
+            txtDisplay.BackColor = Color.Green;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            txtDisplay.Text = "";
+            txtDisplay.BackColor = Color.White;
+        }
+    }
+}
